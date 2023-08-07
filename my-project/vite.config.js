ee24a94cd50e6f757a277814e257bcad34f0594c
amineelakkermi <amineelakkermi37@gmail.com>
2023-08-07 19:19:41 +0200

    Initial commit

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/mon-portfolio/",
   plugins: [react()],
-  base:"mon-portfolio",
  
  
 })
